e42c446783c33c5b1a4a1d3aa193cd7a6e68d5bd
Андрей <andrew-g0lub@yandex.ru>
2023-01-15 22:16:01 +0300

    добавил 3 комментария :)

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -25,17 +25,20 @@ public class DepositCalculator {
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         command = scanner.nextInt();
-        double finalDepositAmount = 0;
+        double finalDepositAmount = 0; /*вот если честно, то я не знаю точно, как лучше сделать, но как по мне, лучше
+        перенести объявление переменной в начало метода и отделить их пустой строкой от отдельного кода, либо вынести за
+        предел метода, вместе с остальными переменными. Хотя, может твой вариант более правильный.
+        Мда, так себе я ревьюер :(*/
         if (command == 1) {
             finalDepositAmount = simplePercent(firstDepositAmount, 0.06, period);
         } else if (command == 2) {
             finalDepositAmount = complexPercent(firstDepositAmount, 0.06, period);
         }
-        System.out.println("Результат вклада: " + firstDepositAmount + " за " + period + " лет превратятся в " + finalDepositAmount);
+        System.out.println("Результат вклада: " + firstDepositAmount + " за " + period + " лет превратятся в " + finalDepositAmount); //эта строка > 120 символов))
     }
 
     public static void main(String[] args) {
-
+//зачем тут эта пустая строка?
         new DepositCalculator().getResultOfDeposit();
     }
 
